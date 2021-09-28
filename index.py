08d752fa7673396b5057312bec9385c91839e063
Master-Ess <67045069+Master-Ess@users.noreply.github.com>
2021-09-29 00:22:29 +1000

    updated index.py with dynamic data
    made index.py show files with the system datetime instead of a simple (5)
    
    the formatting is perfect trust me

--- index.py
+++ index.py
@@ -1,13 +1,18 @@
 from flask import *
 from datetime import datetime
 import re
+from time import *
+from datetime import *
 
 app = Flask(__name__,template_folder='../KLL_207_ASS3')
 
  
 # currently no operation - now there is 
 
-data = str(5)
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+data = str("Current Time =" + current_time)
 
 @app.route("/")
 

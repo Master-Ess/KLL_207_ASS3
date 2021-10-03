f1e59ca9b1d2406fd8ddde19f6cb65662751531f
Master-Ess <67045069+Master-Ess@users.noreply.github.com>
2021-10-04 01:13:09 +1000

    Implemented edit event
    Implemented edit event using same method as edit account
    
    Current Song: Lights Out - Royal Blood

--- website/views.py
+++ website/views.py
@@ -1,9 +1,10 @@
 from operator import countOf
 from os import stat
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, url_for
 from flask.wrappers import Response
 from flask_login import login_required, current_user
 from sqlalchemy import event, update
+from werkzeug.utils import redirect
 from .models import Event, User, Comment, Purchase
 from . import db
 
@@ -30,10 +31,6 @@ class id_purchase:
         self.img = img
 
 user_purchases=[]
-    
-
-
-
 
 @views.route('/', methods=['GET', 'POST'])
 def index():
@@ -56,7 +53,9 @@ def index():
 
 
 @views.route("/book_tickets", methods=['GET', 'POST'])
+@login_required
 def book_ticket():
+    
     if request.method == 'POST':
 
         targetevent= request.form.get('event')
@@ -132,8 +131,14 @@ def make_event():
 
 @views.route("/view_event/<id>")
 def view_event(id):
+    editdata = ""
+
     
+
     alldata = Event.query.filter_by(id=id).first()
+    if current_user.is_authenticated:
+        if int(current_user.id) == int(alldata.user_id):
+            editdata = "Edit Event"
 
     if alldata == None:
         return render_template("404.html")
@@ -144,10 +149,11 @@ def view_event(id):
     tickets = alldata.tickets
     data = alldata.data
     img = alldata.img
+    status = alldata.status
 
     
 
-    return render_template("view_event.html" ,event_name=name , event_location=location ,event_date=date, event_data=data, event_tickets=tickets, event_image=img)
+    return render_template("view_event.html" ,event_name=name , event_location=location ,event_date=date, event_data=data, event_tickets=tickets, event_image=img, edit=editdata, status=status, id=id)
 
 @views.route("/view_previous_purchases")
 @login_required
@@ -194,7 +200,7 @@ def edit_account(id):
     if request.method == 'POST':
         alldata = User.query.filter_by(id=id).first()
 
-
+        #if you submit a NONE it freaks out so you gotta do a bunch of shit like this, i could probably write a def to do it but i cbf
         value = request.form.get('title')
         if value != None:
             alldata.title = value
@@ -225,12 +231,11 @@ def edit_account(id):
             alldata.email = value
             db.session.commit()   
 
-
         return render_template('index.html')
 
     else:
 
-        print(current_user.id)
+        
         if str(current_user.id) != str(id):
             return render_template("403.html")
 
@@ -248,6 +253,77 @@ def edit_account(id):
 
         return render_template("edit_account.html", title=title, fname=fname, lname=lname, dateofbirth=dateofbirth, country=country, email=email )
 
+@views.route("/make_event/<id>", methods=['GET', 'POST'])
+@login_required
+def edit_event(id):
+    if request.method == 'POST':
+        
+        alldata = Event.query.filter_by(id=id).first()
+
+        value = request.form.get('eventname')
+        if value != None:
+            alldata.title = value
+            db.session.commit() 
+
+        value = request.form.get('ntickets')
+        if value != None:
+            alldata.tickets = value
+            db.session.commit()  
+
+        value = request.form.get('status')
+        if value != None:
+            alldata.status = value
+            db.session.commit()          
+
+        value = request.form.get('DOE')
+        if value != None:
+            alldata.date = value
+            db.session.commit() 
+
+        value = request.form.get('URL')
+        if value != None:
+            alldata.img = value
+            db.session.commit()  
+
+        value = request.form.get('cost')
+        if value != None:
+            alldata.ticketcost = value
+            db.session.commit()
+
+        value = request.form.get('location')
+        if value != None:
+            alldata.location = value
+            db.session.commit()
+        
+        value = request.form.get('descript')
+        if value != None:
+            alldata.data = value
+            db.session.commit()
+           
+
+        return redirect(url_for('views.index'))  
+
+        
+    else:
+        alldata = Event.query.filter_by(id=id).first()
 
 
+        if alldata == None:
+            return render_template("404.html")
 
+        if str(current_user.id) != str(alldata.user_id):
+            return render_template("403.html")
+
+
+        name = str(alldata.title)
+        print("marker")
+        print(name)
+        tickets = alldata.tickets
+        status = alldata.status
+        DOE = alldata.date
+        location = alldata.location
+        URL = alldata.img
+        cost = alldata.ticketcost
+        desc = alldata.data
+
+        return render_template("create_event.html", name=name, tickets=tickets, status=status, DOE=DOE, location=location, URL=URL, cost=cost, desc=desc)
